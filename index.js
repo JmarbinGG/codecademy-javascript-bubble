12b93f5af4380a1747452195b42704fb0c764636
JmarbinGG <108483439+JmarbinGG@users.noreply.github.com>
2023-01-18 09:20:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Change the text between the quotation marks and run your code to see the name of your favorite language animated!
 drawText('Hover over me to bubblify!');
-drawText('clone repo and edit index.js to change text!');
 
 // The code below will allow you to interact with the letters
 bounceBubbles();
